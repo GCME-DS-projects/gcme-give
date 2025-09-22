8d2360d69108ffaf0b178369cf957301a9cb7c10
biniyam-29 <bnmmarkos@gmail.com>
2025-09-22 19:18:37 +0300

    contributions

--- api/src/modules/contribution/dto/create-contribution.dto.ts
+++ api/src/modules/contribution/dto/create-contribution.dto.ts
@@ -1,1 +1,30 @@
-export class CreateContributionDto {}
+import { IsUUID, IsOptional, IsNumber, IsString, IsEnum } from 'class-validator';
+import { PAYMENT_METHOD } from '@prisma/client';
+
+export class CreateContributionDto {
+  @IsNumber()
+  amount: number;
+
+  @IsUUID()
+  contributorId: string;
+
+  @IsOptional()
+  @IsUUID()
+  projectId?: string;
+
+  @IsOptional()
+  @IsUUID()
+  missionaryId?: string;
+
+  @IsOptional()
+  @IsUUID()
+  strategyId?: string;
+
+  @IsOptional()
+  @IsEnum(PAYMENT_METHOD)
+  paymentMethod?: PAYMENT_METHOD;
+
+  @IsOptional()
+  @IsString()
+  status?: string; // e.g., PENDING, SUCCESS
+}
