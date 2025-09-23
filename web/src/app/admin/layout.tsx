d41a488a63ef35c0cb051e952b6a92d899a041fa
biniyam-29 <bnmmarkos@gmail.com>
2025-09-23 23:48:16 +0300

    missionaries

--- web/src/app/admin/layout.tsx
+++ web/src/app/admin/layout.tsx
@@ -1,5 +1,5 @@
-import AdminLayout from "./AdminLayout";
-import ReactQueryProvider from "./ReactQueryProvider";
+import AdminLayout from "./admin-layout";
+import ReactQueryProvider from "./react-query-provider";
 
 export default function AdminSectionLayout({ children }: { children: React.ReactNode }) {
   return (
